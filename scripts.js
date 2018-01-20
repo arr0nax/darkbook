8f2d92527a6f2c3109dd1c5c7b48e44e9f24ee0b
Clayton Collins <claytonccollins@gmail.com>
2018-01-19 17:18:50 -0800

    add instructions and file upload

--- scripts.js
+++ scripts.js
@@ -4,14 +4,26 @@ var markers = [
   {lat: -25.362, lng: 131.043},
 ]
 
+document.addEventListener("DOMContentLoaded", function(event) {
+  document.getElementById("files").addEventListener("change",function() {
+    var file = this.files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var text = reader.result;
+      parseIPs(text);
+    }
+
+    reader.readAsText(file);
+  })
+});
+
 function initMap() {
-  console.log('hello');
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 37.7697, lng: -122.3933},
     zoom: 8
   });
   // fetchLatLng();
-  parseIPs();
+  // parseIPs();
 }
 
 function dropMarker(marker) {
@@ -25,49 +37,36 @@ function fetchLatLng(ip) {
   fetch('http://freegeoip.net/json/'+ip).then(function(res) {
     return res.json();
   }).then(function(res) {
-    console.log(res);
     dropMarker({lat: res.latitude, lng: res.longitude});
   });
 }
 
-function parseIPs() {
-  fetch('security.htm').then(function(res) {
-    return res.text();
-  }).then(function(res) {
-    console.log(res);
-    var parser = new DOMParser();
-    var doc = parser.parseFromString(res, "text/html");
-    console.log(doc);
-    var ips = [];
-    var metaData = [];
+function parseIPs(res) {
+  var parser = new DOMParser();
+  var doc = parser.parseFromString(res, "text/html");
+  var ips = [];
+  var metaData = [];
 
 
-    var hTags = doc.getElementsByTagName("h2");
-    console.log(hTags);
-    var searchText = "IP Addresses";
-    var found;
+  var hTags = doc.getElementsByTagName("h2");
+  var searchText = "IP Addresses";
+  var found;
 
-    for (var i = 0; i < hTags.length; i++) {
-      console.log(hTags[i]);
-      if (hTags[i].textContent == searchText) {
-        found = i;
-        break;
-      }
+  for (var i = 0; i < hTags.length; i++) {
+    if (hTags[i].textContent == searchText) {
+      found = i;
+      break;
     }
+  }
 
-    console.log(found);
+  var liips = doc.getElementsByTagName("h2")[found].nextSibling.childNodes;
+  for (var i = 0; i < liips.length; i++) {
+    ips.push(liips[i].innerText);
 
-    var liips = doc.getElementsByTagName("h2")[found].nextSibling.childNodes;
-    console.log(liips);
-    for (var i = 0; i < liips.length; i++) {
-      ips.push(liips[i].innerText);
-      
 
-    }
-    console.log(ips);
-    ips.forEach(ip => {
-      fetchLatLng(ip);
-    });
+  }
+  ips.forEach(ip => {
+    fetchLatLng(ip);
   });
 
 }
